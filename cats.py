46411d69fbae6295213f06c10606ae920fe708a9
Andrew <70990567+apakula036@users.noreply.github.com>
2022-11-08 16:57:18 -0800

    Update for Cats, API broke

--- cats.py
+++ cats.py
@@ -1,18 +1,24 @@
-from locale import format_string
-from urllib import request, response
 import requests
 import config
+import json
+import time
 
 BASE_URL = "https://cat-fact/facts/random"
 
+#https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1
+#https://cat-fact.herokuapp.com/#/facts/random?animal_type=cat&amount=1
+#/facts/random?animal_type=cat&amount=2
+#response = requests.get(BASE_URL)
 
+#if response.status_code == 200:
+# data = response.json()
+#  info = data['text']
 
-response = requests.get(BASE_URL)
+# print(data)
+    
+#else:
+# print("An error occurred.")
 
-if response.status_code == 200:
-    data = response.json()
-    info = data["text"]
-
-    print(data)
-else:
-    print("An error occurred.")
+print("test")
+data = requests.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1')
+print(data.json())
